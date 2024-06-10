2c70613c6d69aa528d32b174791fd55260cde897
WatarizZz-tests <120670228+WatarizZz-tests@users.noreply.github.com>
2024-06-10 11:44:42 +0100

    Update ProtectAdminPanel.js

--- src/components/routeprotection/ProtectAdminPanel.js
+++ src/components/routeprotection/ProtectAdminPanel.js
@@ -1,1 +1,17 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectAdminPanel = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    const PRIME_ACCOUNT_ID = '66646b06e6db92659aa0ac2b';
+
+    if (!user || user._id !== PRIME_ACCOUNT_ID) {
+        return <Navigate to="/" />;
+    }
+
+    return children;
+};
+
+export default ProtectAdminPanel;
 
